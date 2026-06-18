ac6bbadc37a19b54a53e541280019bfcfd271bf6
simproducao <miguelsantos@sim.pt>
2026-06-18 22:20:28 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        sim: {
+          orange: '#E8571A',
+          dark: '#0b0c0f',
+          card: '#1a1b1f',
+          border: '#2a2b2f',
+        }
+      }
+    }
+  },
+  plugins: []
+}
